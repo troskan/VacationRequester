390ceac8471c51c351f6aae45118869d7251e65f
Alvin strandberg <Str4ndb3rg@live.se>
2023-10-09 12:03:32 +0200

    Updated program.cs

---
+++ Middleware/SwaggerAuthentication.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace VacationRequester.Middleware
+{
+    public static class SwaggerAuthentication
+    {
+        public static void UseSwaggerAuthentication(WebApplicationBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Microsoft.OpenApi.Models;
 using VacationRequester.Data;
 using VacationRequester.Repositories.Interfaces;
 using VacationRequester.Repositories;
+using VacationRequester.Middleware;
 
 namespace VacationRequester
 {
@@ -23,34 +24,7 @@ namespace VacationRequester
             builder.Services.AddEndpointsApiExplorer();
 
             //Add bearer token UI
-            builder.Services.AddSwaggerGen(opt =>
-            {
-                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
-                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    In = ParameterLocation.Header,
-                    Description = "Please enter token",
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.Http,
-                    BearerFormat = "JWT",
-                    Scheme = "bearer"
-                });
-
-                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type=ReferenceType.SecurityScheme,
-                    Id="Bearer"
-                }
-            },
-            new string[]{}
-        }
-    });
-            });
+            SwaggerAuthentication.UseSwaggerAuthentication(builder);
 
             //Add Cors
             builder.Services.AddCors(options =>
