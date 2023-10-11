4f2dc2195fd428fbe33c7e1f8814284d60e0b706
Alvin strandberg <Str4ndb3rg@live.se>
2023-10-11 10:33:05 +0200

    Fixed/added LeaveTypeController, UserCon / LeaveTypeCon

--- Controllers/LeaveRequestController.cs
+++ Controllers/LeaveRequestController.cs
@@ -1,11 +1,112 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using VacationRequester.Models;
+using VacationRequester.Repositories.Interfaces;
 
-namespace VacationRequester.Controllers
+namespace VacationRequester.Controllers;
+[ApiController]
+[Route("[Controller]")]
+public class LeaveRequestController : ControllerBase
 {
-    [ApiController]
-    [Route("[Controller]")]
-    public class LeaveRequestController : ControllerBase
-    {
+    private readonly IRepository<LeaveRequest> _repository;
 
+    public LeaveRequestController(IRepository<LeaveRequest> repository)
+    {
+        _repository = repository;
+    }
+
+    [Authorize]
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var leaveRequests = await _repository.GetAllAsync();
+
+        if (leaveRequests == null || !leaveRequests.Any())
+        {
+            return NotFound();
+        }
+
+        return Ok(leaveRequests);
+    }
+
+    [Authorize]
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById(Guid Id)
+    {
+        if (Id == Guid.Empty)
+        {
+            return null;
+        }
+
+        LeaveRequest leaveRequest = await _repository.GetByIdAsync(Id);
+
+        if (leaveRequest == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(leaveRequest);
+    }
+
+    [Authorize]
+    [HttpPost]
+    public async Task<IActionResult> Create(LeaveRequest leaveRequestToCreate)
+    {
+        if(leaveRequestToCreate == null)
+        {
+            return BadRequest();
+        }
+
+        await _repository.AddAsync(leaveRequestToCreate);
+        return CreatedAtAction(nameof(Create), leaveRequestToCreate.Id);
+    }
+
+    [Authorize]
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(Guid id, LeaveRequest leaveRequestToUpdate)
+    {
+        if (leaveRequestToUpdate == null || id != leaveRequestToUpdate.Id)
+        {
+            return BadRequest();
+        }
+
+        var existingLeaveRequest = await _repository.GetByIdAsync(id);
+
+        if (existingLeaveRequest == null)
+        {
+            return NotFound();
+        }
+
+        existingLeaveRequest.User = leaveRequestToUpdate.User;
+        existingLeaveRequest.UserId = leaveRequestToUpdate.UserId;
+        existingLeaveRequest.LeaveType = leaveRequestToUpdate.LeaveType;
+        existingLeaveRequest.LeaveTypeId = leaveRequestToUpdate.LeaveTypeId;
+        existingLeaveRequest.StartDate = leaveRequestToUpdate.StartDate;
+        existingLeaveRequest.EndDate = leaveRequestToUpdate.EndDate;
+        existingLeaveRequest.DateRequested = leaveRequestToUpdate.DateRequested;
+        existingLeaveRequest.ApprovalState = leaveRequestToUpdate.ApprovalState;
+
+        await _repository.UpdateAsync(existingLeaveRequest);
+        return NoContent();
+    }
+
+    [Authorize]
+    [HttpDelete]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        if (id == Guid.Empty)
+        {
+            return BadRequest();
+        }
+
+        var leaveRequestToDelete = await _repository.GetByIdAsync(id);
+
+        if (leaveRequestToDelete == null)
+        {
+            return NotFound();
+        }
+
+        await _repository.DeleteAsync(leaveRequestToDelete);
+        return Ok();
     }
 }
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,11 +1,52 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using VacationRequester.Models;
+using VacationRequester.Repositories.Interfaces;
 
 namespace VacationRequester.Controllers
 {
     [ApiController]
-    [Route("UserController")]
+    [Route("[controller]")]
     public class UserController : ControllerBase
     {
+        private readonly IRepository<User> _userRepository;
+
+        public UserController(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [Authorize]
+        [HttpGet, Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _userRepository.GetAllAsync();
+            if (users == null || !users.Any())
+            {
+                return NotFound("No users found.");
+            }
+            return Ok(users);
+        }
+
+        [Authorize]
+        [HttpGet("{id}"), Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetUserById(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return BadRequest("Invalid ID.");
+            }
+
+            var user = await _userRepository.GetByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound($"No user found with ID: {id}");
+            }
+
+            return Ok(user);
+        }
 
     }
 }
