e469e0d1f5c9cbb63d75ffb981c581313319e5a5
Charlie Skibsted <charlieskibsted@gmail.com>
2023-10-17 14:20:31 +0200

    Fixed ÅÄÖ

--- Models/Dto/RegisterDto.cs
+++ Models/Dto/RegisterDto.cs
@@ -10,10 +10,10 @@ namespace VacationRequester.Models.Dto
         [StringLength(20, MinimumLength = 4)]
         public string Password { get; set; } = string.Empty;
 
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "First name must only contain letters.")]
+        [RegularExpression(@"^[a-zA-ZÅÄÖåäö]+$", ErrorMessage = "First name must only contain letters.")]
         public string FirstName { get; set; } = string.Empty;
 
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Last name must only contain letters.")]
+        [RegularExpression(@"^[a-zA-ZÅÄÖåäö]+$", ErrorMessage = "Last name must only contain letters.")]
         public string LastName { get; set; } = string.Empty;
     }
 }
