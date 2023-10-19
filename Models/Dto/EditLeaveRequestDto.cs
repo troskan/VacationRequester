8ce2d5a4616b1c0fe4cd70f2d01015fbcc1ba841
Alvin strandberg <Str4ndb3rg@live.se>
2023-10-19 12:04:30 +0200

    DTO

--- Models/Dto/EditLeaveRequestDto.cs
+++ Models/Dto/EditLeaveRequestDto.cs
@@ -2,12 +2,10 @@
 
 namespace VacationRequester.Models
 {
-    public class LeaveRequest
+    public class EditLeaveRequestDto
     {
         public Guid Id { get; set; }
-        public User User { get; set; }
         public Guid UserId { get; set; }
-        public LeaveType LeaveType { get; set; }
         public Guid LeaveTypeId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
